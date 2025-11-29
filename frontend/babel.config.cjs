2d7882c112de01ebdd622d8591dbf4e16853574f
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-11-29 15:09:02 +0100

    Update babel config to parse ES6 modules

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -1,6 +1,28 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    [
+      '@babel/preset-env',
+      {
+        targets: { node: 'current' },
+        // Transform ES6 modules to CommonJS for Jest
+        modules: 'commonjs',
+      },
+    ],
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: { node: 'current' },
+            // Ensure ES6 modules are transformed to CommonJS in test environment
+            modules: 'commonjs',
+          },
+        ],
+        ['@babel/preset-react', { runtime: 'automatic' }],
+      ],
+    },
+  },
 };
